50af2bd55b79e21be8244fbc38788fd462b16aa4
RaulRangelxD <raulrangel1230@gmail.com>
2024-04-29 23:54:05 -0400

    agregado bootstrap al edit

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,4 +5,10 @@ class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
         fields = ['nombre', 'precio', 'descripcion', 'imagen']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'precio': forms.TextInput(attrs={'class': 'form-control'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control'}),
+            'imagen': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
 
