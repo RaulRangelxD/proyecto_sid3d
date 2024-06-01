d59615d5192b4ab3c889072e2eef777089601fc0
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 <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>
2024-06-01 00:53:43 -0400

    Esquema de base de datos

--- productos/models.py
+++ productos/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE, default=1)  
     precio = models.DecimalField(max_digits=10, decimal_places=2)
+    precio_2 = models.DecimalField(max_digits=10, decimal_places=2,default=0)
+    precio_3 = models.DecimalField(max_digits=10, decimal_places=2,default=0)
     descripcion = models.TextField()
     imagen = models.ImageField(upload_to='productos/')
+    existencias = models.IntegerField(default=0)
 
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
--- project/settings.py
+++ project/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'proyecto',
         'USER': 'postgres',
-        'PASSWORD': 'alejandra',
+        'PASSWORD': 'jema2019',
         'HOST': 'localhost',
         'PORT': '5432',
     }
