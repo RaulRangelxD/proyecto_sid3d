3ec3578d7b1964481b8ab4ee4bbf34b5a5fa8452
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 <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>
2024-06-10 22:16:08 -0400

    Modelos

--- productos/models.py
+++ productos/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class Producto(models.Model):
-    nombre = models.CharField(max_length=100)
+    id = models.AutoField(primary_key=True)
+    codigo = models.CharField(max_length=255, default='1')
+    nombre = models.CharField(max_length=255)
     categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE, default=1)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     precio_2 = models.DecimalField(max_digits=10, decimal_places=2)
@@ -9,9 +11,48 @@ class Producto(models.Model):
     descripcion = models.TextField()
     imagen = models.ImageField(upload_to='productos/')
     existencias = models.IntegerField()
-
+    fecha = models.DateField(auto_now=True)
+    
 class Categoria(models.Model):
+    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
+   
+class Venta(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_producto = models.ForeignKey('Producto', on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha = models.DateField(auto_now_add=True)
+    cedula = models.CharField(max_length=255)
+    cliente_venta = models.CharField(max_length=255)
+    sector = models.CharField(max_length=255)
+    telefono = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.nombre
+class Compra(models.Model):
+    id = models.AutoField(primary_key=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    id_producto = models.ForeignKey('Producto', on_delete=models.CASCADE)
+    fecha = models.DateField(auto_now_add=True)
+    proveedor = models.CharField(max_length=255)
+
+class Ingresos(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_venta = models.ForeignKey('Venta', on_delete=models.CASCADE)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha = models.DateField(auto_now_add=True)
+
+class Egresos(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_compra = models.ForeignKey('Compra', on_delete=models.CASCADE)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha = models.DateField(auto_now_add=True)
+
+class Factura(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_venta = models.ForeignKey('Venta', on_delete=models.CASCADE)
+    fecha = models.DateField(auto_now_add=True)
+
+
+class Cliente(models.Model):
+   id = models.AutoField(primary_key=True)
+   cliente_cliente = models.ForeignKey('Venta', on_delete=models.CASCADE)
--- project/settings.py
+++ project/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'proyecto',
         'USER': 'postgres',
-        'PASSWORD': 'alejandra',
+        'PASSWORD': 'jema2019',
         'HOST': 'localhost',
         'PORT': '5432',
     }
