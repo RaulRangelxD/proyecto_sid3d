41aa72d0480f9a0cc60989e8f869d4836034a82e
RaulRangelxD <raulrangel1230@gmail.com>
2024-08-04 19:03:27 -0400

    intentando arreglar los statics 3ra vez

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,8 @@ MEDIA_URL = '/media/'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower == 'true'
+# DEBUG = os.environ.get('DEBUG', 'False').lower == 'true'
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
